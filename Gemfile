ef4f2d95c662ee71b40b637ca18bc7f7a526ef27
Chris Postma <cjrpostma@gmail.com>
2019-08-06 09:48:35 -0700

    Installs devise, views, pages controller, basic setup

--- Gemfile
+++ Gemfile
@@ -53,3 +53,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "devise", "~> 4.6"
+
+gem "webpacker", "~> 4.0"
+
+gem "react-rails", "~> 2.6"
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get '*path', to: 'pages#root', constraints: ->(request){ request.format.html? }
+  root to: "root#index"
 end
