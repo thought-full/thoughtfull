6faa4c120ee59939a7e0edeb23b5bdd68f15effc
Chris Postma <cjrpostma@gmail.com>
2019-08-06 16:46:04 -0700

    adds incomplete new post component with form

--- app/javascript/components/pages/NewPost.js
+++ app/javascript/components/pages/NewPost.js
@@ -1,5 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Form, Button } from 'react-bootstrap'
+
 class NewPost extends React.Component {
   constructor(props) {
     super(props)
@@ -7,7 +9,7 @@ class NewPost extends React.Component {
       form: {
         date: "",
         body: "",
-        public_view: ""
+        public_view: false
       }
     }
   }
@@ -18,11 +20,50 @@ class NewPost extends React.Component {
     onSubmit(form)
   }
 
+  onChange = (e) => {
+    const { form } = this.state
+    const { name, value } = e.target
+    form[name] = value
+    this.setState({ form })
+  }
+
+  togglesPublicView = () => {
+    const { form } = this.state
+    const { public_view } = form
+    form.public_view = public_view === false ? true : false;
+    this.setState({ form })
+  }
+
   render () {
+    const { date, body, public_view } = this.state.form
     return (
       <React.Fragment>
         <h1>Create a New Post</h1>
-        <button onClick={this.localSubmit}>Submit</button>
+
+        <Form>
+          <Form.Group>
+            <Form.Label>Date</Form.Label>
+            <Form.Control onChange={this.onChange} name="date" value={date} type="text" placeholder="Enter date" />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Body</Form.Label>
+            <Form.Control onChange={this.onChange} name="body" value={body} type="text" placeholder="Enter thought" />
+          </Form.Group>
+
+          <div className="custom-control custom-switch">
+            <input onChange={this.togglesPublicView} name="public_view" type="checkbox" className="custom-control-input" id="customSwitch1" checked={public_view}></input>
+            <label className="custom-control-label" htmlFor="customSwitch1">Make thought private?</label>
+          </div>
+
+
+
+
+          <Button onClick={this.localSubmit} variant="primary">
+            Submit
+          </Button>
+        </Form>
+
       </React.Fragment>
     );
   }
