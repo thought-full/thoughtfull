13ee0f46a9e05651c5ea0ab4821768a8c56e7ee5
Randy Tseng <randytseng@Randys-MacBook-Air.local>
2019-08-09 11:27:21 -0700

    fixed random merge conflicts

--- app/javascript/components/pages/Private.js
+++ app/javascript/components/pages/Private.js
@@ -1,33 +1,44 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Link } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 
-class Private extends React.Component {
+class Posts extends React.Component {
   render() {
     const { posts, currentUserId, deletePost } = this.props
     return (
       <React.Fragment>
+      <Container>
       <h1>Posts</h1>
-      <ul>
-        {posts.reduce((filtered, post) => {
-            if(post.public_view == false && post.user_id === currentUserId){
-                filtered.push(
-                    <li
-                    key={post.id}>
-                        {post.body}
-                        <div>
-                            <button onClick={() => deletePost(post.id)}>Delete Post</button>
-                            <Link to={`/edit/${post.id}`}>Edit</Link>
-                        </div>
-                    </li>
-                )
-            }
-            return filtered
-        }, [])}
-      </ul>
+      <div className="card-flex">
+      {posts.reduce((filtered, post) => {
+        if(post.public_view == false && post.user_id === currentUserId){
+          filtered.push(
+            <div 
+              className="card border-primary mb-3 card-width"
+              key={post.id}
+            >
+              <div className="card-header">Header</div>
+              <div className="card-body">
+              <h4 className="card-title">Primary card title</h4>
+              <p className="card-text">
+                {post.body}
+                  <div>
+                    <button onClick={() => deletePost(post.id)}>Delete Post</button>
+                    <Link to={`/edit/${post.id}`}>Edit</Link>
+                  </div>
+              </p>
+              </div>
+            </div>
+          )
+        }
+        return filtered
+      }, [])}
+      </div>
+      </Container>
     </React.Fragment>
     )
   }
 }
 
-export default Private
+export default Posts
