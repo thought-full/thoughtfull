775eb1a65859816156d079cd29110f07459e0673
Chris Postma <cjrpostma@gmail.com>
2019-08-12 15:55:42 -0700

    Adds SignIn component - unfinished

--- app/javascript/components/MainApp.js
+++ app/javascript/components/MainApp.js
@@ -1,99 +1,93 @@
-import React from "react"
-import PropTypes from "prop-types"
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Switch
-} from 'react-router-dom'
+import React from "react";
+import PropTypes from "prop-types";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
-import Jumbotron from './components/Jumbotron'
-import Navigation from './components/Navigation'
-import Posts from './pages/Posts'
-import Location from './pages/Location'
-import NewPost from './pages/NewPost'
-import EditPost from './pages/EditPost'
-import Private from './pages/Private'
+import Jumbotron from "./components/Jumbotron";
+import Navigation from "./components/Navigation";
+import Posts from "./pages/Posts";
+import Location from "./pages/Location";
+import NewPost from "./pages/NewPost";
+import EditPost from "./pages/EditPost";
+import Private from "./pages/Private";
+import SignIn from "./components/SignIn";
 
 class MainApp extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       posts: [],
-      error: null,
-    }
-    this.getPosts()
+      error: null
+    };
+    this.getPosts();
   }
 
   getPosts = () => {
     fetch("/posts")
       .then(response => {
-        return response.json()
+        return response.json();
       })
       .then(posts => {
-        this.setState({ posts })
-      })
-  }
+        this.setState({ posts });
+      });
+  };
 
-  createPost = (attrs) => {
+  createPost = attrs => {
     console.log("Working. These are the attrs", attrs);
     return fetch("/posts", {
-      method: 'POST',
-      headers:{
+      method: "POST",
+      headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({post: attrs})
-    })
-    .then(response => {
-      if(response.status === 201){
-        this.getPosts()
+      body: JSON.stringify({ post: attrs })
+    }).then(response => {
+      if (response.status === 201) {
+        this.getPosts();
       }
-    })
-  }
+    });
+  };
 
-  showPost = (id) => {
-    return fetch(`/posts/${id}`)
-    .then(response => {
-      return response.json()
-    })
-  }
+  showPost = id => {
+    return fetch(`/posts/${id}`).then(response => {
+      return response.json();
+    });
+  };
 
   editPost = (id, attrs) => {
     console.log("editing", id, attrs);
     return fetch(`/posts/${id}`, {
-      method: 'PATCH',
-      headers:{
+      method: "PATCH",
+      headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({post: attrs})
+      body: JSON.stringify({ post: attrs })
     })
-    .then(response => {
-      if(response.status === 202){
-        this.getPosts()
-      }else{
-        return response.json()
-      }
-    })
-    .then(payload => {
-      this.setState({error: payload.error})
-    })
-  }
+      .then(response => {
+        if (response.status === 202) {
+          this.getPosts();
+        } else {
+          return response.json();
+        }
+      })
+      .then(payload => {
+        this.setState({ error: payload.error });
+      });
+  };
 
-  deletePost = (id) => {
+  deletePost = id => {
     return fetch(`/posts/${id}`, {
-      method: 'DELETE'
+      method: "DELETE"
     })
-    .then(response => {
-      if(response.status === 200){
-        this.getPosts()
-      }else{
-        return response.json()
-      }
-    })
-    .then(payload => {
-      this.setState({error: payload.error})
-    })
-  }
+      .then(response => {
+        if (response.status === 200) {
+          this.getPosts();
+        } else {
+          return response.json();
+        }
+      })
+      .then(payload => {
+        this.setState({ error: payload.error });
+      });
+  };
 
   render() {
     const {
@@ -101,9 +95,9 @@ class MainApp extends React.Component {
       sign_in_route,
       sign_out_route,
       current_user_id
-    } = this.props
+    } = this.props;
 
-    const { posts, error } = this.state
+    const { posts, error } = this.state;
 
     return (
       <React.Fragment>
@@ -113,90 +107,81 @@ class MainApp extends React.Component {
           logged_in={this.props.logged_in}
           sign_in_route={this.props.sign_in_route}
           sign_out_route={this.props.sign_out_route}
-          />
+        />
 
-        {error &&
-          <h2>{error}</h2>
-        }
+        {error && <h2>{error}</h2>}
 
         <Router>
-
           <Route
             exact
             path="/"
-            render={(props) => {
+            render={props => {
               return (
                 <Posts
                   {...props}
-                  currentUserId = {current_user_id}
-                  posts = {posts}
-                  deletePost = {this.deletePost}
+                  currentUserId={current_user_id}
+                  posts={posts}
+                  deletePost={this.deletePost}
                 />
-              )
+              );
             }}
           />
 
           <Route
             path="/location"
-            render={(props) => {
+            render={props => {
               return (
                 <Location
                   {...props}
-                  currentUserId = {current_user_id}
-                  posts = {posts}
-                  deletePost = {this.deletePost}
+                  currentUserId={current_user_id}
+                  posts={posts}
+                  deletePost={this.deletePost}
                 />
-              )
+              );
             }}
           />
 
           <Route
             path="/private"
-            render={(props) => {
-              return(
+            render={props => {
+              return (
                 <Private
                   {...props}
-                  currentUserId = {current_user_id}
-                  posts = {posts}
-                  deletePost = {this.deletePost}
+                  currentUserId={current_user_id}
+                  posts={posts}
+                  deletePost={this.deletePost}
                 />
-              )
+              );
             }}
           />
 
-          {logged_in &&
+          {logged_in && (
             <Switch>
               <Route
                 path="/new"
-                render={ (props) => {
-                  return(
-                    <NewPost
-                    {...props}
-                    createPost={this.createPost}
-                    />
-                  )
+                render={props => {
+                  return <NewPost {...props} createPost={this.createPost} />;
                 }}
               />
 
               <Route
                 path="/edit/:id"
-                render={(props) => {
-                  return(
+                render={props => {
+                  return (
                     <EditPost
-                    {...props}
-                    editPost={this.editPost}
-                    showPost={this.showPost}
+                      {...props}
+                      editPost={this.editPost}
+                      showPost={this.showPost}
                     />
-                  )
+                  );
                 }}
               />
             </Switch>
-          }
+          )}
         </Router>
-
       </React.Fragment>
     );
   }
 }
 
-export default MainApp
+export default MainApp;
