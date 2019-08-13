5ea4fa7fb44c7242c0e02faaca7bada74572ece6
Jason <jason@Jasons-MacBook-Pro.local>
2019-08-13 10:12:05 -0700

    adds logo in navbar

--- app/javascript/components/components/Navigation.js
+++ app/javascript/components/components/Navigation.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types"
 import {
   Navbar,
   Nav,
-  Container
+  Container,
 } from 'react-bootstrap'
-
+import Logo from './images/tf_icon_transparent.png'
 
 class Navigation extends React.Component {
 
@@ -20,6 +20,11 @@ class Navigation extends React.Component {
     return (
       <React.Fragment>
           <Navbar bg="light" sticky="top">
+            
+            <Nav.Link href="/">
+            <img src= {Logo} style= {{margin: -15, width: 45, height: 45}} href="/"></img>
+            </Nav.Link>
+
             <Nav.Link href="/">Posts</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
 
