0304793cda37961e8826982e3a0bef18981a3acb
Jason <jason@Jasons-MacBook-Pro.local>
2019-08-13 10:46:41 -0700

    styles jumbotron and navbar

--- app/javascript/components/components/Navigation.js
+++ app/javascript/components/components/Navigation.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types"
 import {
   Navbar,
   Nav,
-  Container,
 } from 'react-bootstrap'
 import Logo from './images/tf_icon_transparent.png'
 
@@ -19,28 +18,26 @@ class Navigation extends React.Component {
 
     return (
       <React.Fragment>
-          <Navbar bg="light" sticky="top">
-            
+          <Navbar bg="primary" sticky="top">
             <Nav.Link href="/">
-            <img src= {Logo} style= {{margin: -15, width: 45, height: 45}} href="/"></img>
+            <img src= {Logo} style= {{margin: -15, width: 45, height: 45}}></img>
             </Nav.Link>
-
-            <Nav.Link href="/">Posts</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link style= {{color: "white"}} href="/">Posts</Nav.Link>
+            <Nav.Link style= {{color: "white"}} href="/about">About</Nav.Link>
 
             {logged_in &&
-              <Nav.Link href="/new">New Post</Nav.Link>
+              <Nav.Link style= {{color: "white"}} href="/new">New Post</Nav.Link>
             }
 
             {logged_in &&
-              <Nav.Link href="/private">Private Posts</Nav.Link>
+              <Nav.Link style= {{color: "white"}} href="/private">Private Posts</Nav.Link>
             }
 
             {logged_in &&
-              <Nav.Link href={sign_out_route}>Sign Out</Nav.Link>
+              <Nav.Link style= {{color: "white"}} href={sign_out_route}>Sign Out</Nav.Link>
             }
             {!logged_in &&
-              <Nav.Link href={sign_in_route}>Sign In</Nav.Link>
+              <Nav.Link style= {{color: "white"}} href={sign_in_route}>Sign In</Nav.Link>
             }
         </Navbar>
       </React.Fragment>
