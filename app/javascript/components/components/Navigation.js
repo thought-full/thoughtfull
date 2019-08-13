aba6fd42536e19c1265c35c7b2baf7aee1de6e14
Chris Postma <cjrpostma@gmail.com>
2019-08-13 14:44:32 -0700

    adds nav hover style

--- app/javascript/components/components/Navigation.js
+++ app/javascript/components/components/Navigation.js
@@ -1,48 +1,60 @@
-import React from "react"
-import PropTypes from "prop-types"
-import {
-  Navbar,
-  Nav,
-} from 'react-bootstrap'
-import Logo from './images/tf_icon_transparent.png'
+import React from "react";
+import PropTypes from "prop-types";
+import { Navbar, Nav } from "react-bootstrap";
+import Logo from "./images/tf_icon_transparent.png";
+import "./navigation.scss";
 
 class Navigation extends React.Component {
-
   render() {
     const {
       logged_in,
       sign_in_route,
       sign_out_route,
       current_user_id
-    } = this.props
+    } = this.props;
 
     return (
       <React.Fragment>
-          <Navbar bg="primary" sticky="top">
-            <Nav.Link href="/">
-            <img src= {Logo} style= {{margin: -15, width: 45, height: 45}}></img>
+        <Navbar bg="primary" sticky="top">
+          <Nav.Link href="/">
+            <img
+              src={Logo}
+              style={{ margin: -15, width: 45, height: 45 }}
+            ></img>
+          </Nav.Link>
+          <Nav.Link style={{ color: "white" }} href="/" className="one">
+            Posts
+          </Nav.Link>
+          <Nav.Link style={{ color: "white" }} href="/about" className="two">
+            About
+          </Nav.Link>
+
+          {logged_in && (
+            <Nav.Link style={{ color: "white" }} href="/new" className="three">
+              New Post
             </Nav.Link>
-            <Nav.Link style= {{color: "white"}} href="/">Posts</Nav.Link>
-            <Nav.Link style= {{color: "white"}} href="/about">About</Nav.Link>
+          )}
 
-            {logged_in &&
-              <Nav.Link style= {{color: "white"}} href="/new">New Post</Nav.Link>
-            }
+          {logged_in && (
+            <Nav.Link style={{ color: "white" }} href="/private">
+              Private Posts
+            </Nav.Link>
+          )}
 
-            {logged_in &&
-              <Nav.Link style= {{color: "white"}} href="/private">Private Posts</Nav.Link>
-            }
-
-            {logged_in &&
-              <Nav.Link style= {{color: "white"}} href={sign_out_route}>Sign Out</Nav.Link>
-            }
-            {!logged_in &&
-              <Nav.Link style= {{color: "white"}} href={sign_in_route}>Sign In</Nav.Link>
-            }
+          {logged_in && (
+            <Nav.Link style={{ color: "white" }} href={sign_out_route}>
+              Sign Out
+            </Nav.Link>
+          )}
+          {!logged_in && (
+            <Nav.Link style={{ color: "white" }} href={sign_in_route}>
+              Sign In
+            </Nav.Link>
+          )}
         </Navbar>
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default Navigation
+export default Navigation;
