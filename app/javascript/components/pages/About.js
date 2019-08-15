82b49b6dacc3a4ee738cb5dcf1d756f77547e965
Jason <jason@Jasons-MacBook-Pro.local>
2019-08-15 16:07:34 -0700

    added logo to and styled about me

--- app/javascript/components/pages/About.js
+++ app/javascript/components/pages/About.js
@@ -5,26 +5,27 @@ import Dylan from "../components/images/dylan.jpeg";
 import Jason from "../components/images/jason.jpeg";
 import Randy from "../components/images/randy.png";
 import Github from "../components/images/githublogo.svg";
-import LinkedIn from "../components/images/linkedinlogo.png"
-
+import LinkedIn from "../components/images/linkedinlogo.png";
+import Logo from "../components/images/tf_logo_white_bkgd.png";
+import {Redirect} from 'react-router-dom'
 
 class About extends React.Component {
   render() {
     return (
       <React.Fragment>
         <br />
-        <hr style={{ margin: "0 15%" }}/>
+        <hr style={{ margin: "0 15%" }} />
         <br />
         <h2 style={{ textAlign: "center" }}>Our Vision</h2>
         <p style={{ textAlign: "justified", margin: "0 15%" }}>  Each one of our lives is filled of small moments of significance: of transformation and realization. However, our minds are crowded with the endless distractions of today's world, and we neglect to capture these meaningful stories. Here at ThoughtFull, our goal is to provide a platform for users to take some time each day to ask, "What made today different than any other?", to recollect the little moments, and to discover what is truly significant. </p>
         <br />
-        <hr style={{ margin: "0 15%" }}/>
+        <hr style={{ margin: "0 15%" }} />
         <br />
         <h2 style={{ textAlign: "center" }}>Our Story</h2>
         <p style={{ textAlign: "justified", margin: "0 15%" }}> ThoughtFull began in August of 2019 as a capstone project for a three-month long Web Developer bootcamp in downtown San Diego. It has since grown from a idea formed during a brainstorm into a complete project, and we look forward to how ThoughtFull will continue to grow in the future.</p>
         <p style={{ textAlign: "justified", margin: "0 15%" }}>Today, ThoughtFull has been deployed, and the team is continuing to add features that will improve the channels with which our users interact with one another and with their own stories in meaningful ways.</p>
         <br />
-        <hr style={{ margin: "0 15%" }}/>
+        <hr style={{ margin: "0 15%" }} />
         <br />
         <h2 style={{ textAlign: "center" }}>We Are ThoughtFull:</h2>
         <br />
@@ -83,7 +84,12 @@ class About extends React.Component {
             </Card.Body>
           </Card>
         </CardDeck>
-        <hr style={{ margin: "0 15%" }}/>
+        <hr style={{ margin: "0 15%" }} />
+        <br/>
+        <br/>
+        <img src={Logo} style={{ width: "25%", display: "block", margin: "auto" }}></img>
+        <br/>
+        <br/>
       </React.Fragment >
     )
   }
